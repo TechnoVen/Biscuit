0ecfc0427fb9ce24defa7a0b48a551ffb25678b5
laed37 <La.Edwin37@gmail.com>
2017-01-10 13:40:50 -0800

    Create initial frontend components

---
+++ frontend/components/Root.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
+import App from './App';
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={App}/>
+    </Router>
+  </Provider>
+);
+
+export default Root;

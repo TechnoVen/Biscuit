1b99a0dae93aeb639ee7957fdd11e057bb5ade92
laed37 <La.Edwin37@gmail.com>
2017-01-10 13:40:09 -0800

    Add webpack modules

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/KibbleWithStrangers.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  devtool: 'source-maps',
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};

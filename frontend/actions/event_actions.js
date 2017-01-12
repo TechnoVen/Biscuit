8f34a26dbe5308ee403a7adf5135fda00359cc14
laed37 <La.Edwin37@gmail.com>
2017-01-11 17:32:31 -0800

    Create redux event api

---
+++ frontend/actions/event_actions.js
@@ -0,0 +1,34 @@
+import * as EventAPIUtil from '../actions/event_actions';
+
+export const RECEIVE_EVENT = 'RECEIVE_EVENT';
+export const REMOVE_EVENT = 'REMOVE_EVENT';
+
+export const receiveEvent = (event) => ({
+  type: RECEIVE_EVENT,
+  event
+});
+
+export const removeEvent = (event) => ({
+  type: REMOVE_EVENT,
+  event
+});
+
+export const createEvent = (event) => (dispatch) => (
+  EventAPIUtil.createEvent(event)
+    .then(response => dispatch(receiveEvent(response)))
+);
+
+export const updateEvent = (event) => (dispatch) => (
+  EventAPIUtil.updateEvent(event)
+    .then(response => dispatch(receiveEvent(response)))
+);
+
+export const fetchEvent = (eventId) => (dispatch) => (
+  EventAPIUtil.fetchEvent(eventId)
+    .then(response => dispatch(receiveEvent(response)))
+);
+
+export const deleteEvent = (eventId) => (dispatch) => (
+  EventAPIUtil.deleteEvent(eventId)
+    .then(response => dispatch(removeEvent(response)))
+);
---
+++ frontend/util/event_api_util.js
@@ -0,0 +1,29 @@
+export const createEvent = (event) => (
+  $.ajax({
+    method: 'POST',
+    url: '/api/events',
+    data: {event}
+  })
+);
+
+export const fetchEvent = (eventId) => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/events/${eventId}`
+  })
+);
+
+export const updateEvent = (event) => (
+  $.ajax({
+    method: 'PATCH',
+    url: `/api/events/${event.id}`,
+    data: {event}
+  })
+);
+
+export const deleteEvent = (eventId) => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/events/${eventId}`
+  })
+);

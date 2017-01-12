11709c6f5c83ce37a629b263a66da6a3b2ab25d9
laed37 <La.Edwin37@gmail.com>
2017-01-11 19:41:48 -0800

    Add annotate

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: hosts
+#
+#  id      :integer          not null, primary key
+#  desc_1  :text
+#  desc_2  :text
+#  desc_3  :text
+#  user_id :integer          not null
+#
+
 class Host < ApplicationRecord
   validates :user_id, presence: true
 

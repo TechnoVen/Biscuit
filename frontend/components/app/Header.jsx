1df2f69d11db4a47ab75d419f24237d7a6268a60
laed37 <La.Edwin37@gmail.com>
2017-01-13 17:07:44 -0800

    Add svg graphics

--- frontend/components/app/Header.jsx
+++ frontend/components/app/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router';
 import HeaderNavLinks from './HeaderNavLinks';
+import {BiscuitLogo} from '../../svg/svg_graphics';
 
 const Header = ({signedIn, handleSignOut}) => {
   return (
     <header>
       <div className="header-container">
-        <Link className="header-logo" to='/'>kibble with strangers</Link>
+        <Link className="header-logo-link" to='/'>
+          <div className="logo-container">
+            <BiscuitLogo
+              viewBox="-10 -10 480 480"
+              height="50px"
+              width="50px"
+              stroke="white"
+              strokeWidth={20}
+            />
+            biscuit
+          </div>
+        </Link>
         <HeaderNavLinks signedIn={signedIn} handleSignOut={handleSignOut}/>
       </div>
     </header>

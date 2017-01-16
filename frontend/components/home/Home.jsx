ec955fc1385e994636b66167c68aba3e79ca5433
laed37 <La.Edwin37@gmail.com>
2017-01-15 18:45:51 -0800

    Change to Link

--- frontend/components/home/Home.jsx
+++ frontend/components/home/Home.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
-import {hashHistory} from 'react-router';
+import {Link} from 'react-router';
 
-export default class Home extends React.Component {
-  constructor() {
-    super();
+const Home = () => (
+  <div className="home-container">
+    <div className="home-splash">
+      <h1>Fact: Pets make people smile.</h1>
+      <span>
+        They also ruin your furniture unless you take them outside to play.
+      </span>
+      <Link to="/signup">
+        <button className="hvr-fade">join a play date</button>
+      </Link>
+    </div>
+  </div>
+);
 
-    this.handleSplashButton = this.handleSplashButton.bind(this);
-  }
-
-  handleSplashButton() {
-    hashHistory.push('/signup');
-  }
-
-  render() {
-
-    return (
-      <div className="home-container">
-        <div className="home-splash">
-          <h1>Fact: Pets make people smile.</h1>
-          <span>
-            They also ruin your furniture unless you take them outside to play.
-          </span>
-          <button onClick={this.handleSplashButton} className="hvr-fade">
-            join a play date
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+export default Home;

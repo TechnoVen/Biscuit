7491734f61daddf5576e91026eac563e5b6b3193
laed37 <La.Edwin37@gmail.com>
2017-01-16 21:07:25 -0800

    Create event card

---
+++ frontend/components/EventCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class EventCard extends React.Component {
+  render() {
+    return (
+      <div>
+        EventCard Goes Here
+      </div>
+    );
+  }
+}

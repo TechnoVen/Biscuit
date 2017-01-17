c4719b962dc0ab9cdd58f8143ceeeaab92944cb8
laed37 <La.Edwin37@gmail.com>
2017-01-17 12:58:58 -0800

    Remove window const

--- frontend/Biscuit.jsx
+++ frontend/Biscuit.jsx
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom';
 import Root from './components/Root';
 import configureStore from './store/store';
 import EventForm from './components/hosting/EventForm';
+import moment from 'moment';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
-    const preloadedState = {session: {currentUser: window.currentUser}};
+    const preloadedState = {session: {
+      currentUser: window.currentUser,
+      hostProfile: null,
+      events: {},
+      errors: []
+    }};
     store = configureStore(preloadedState);
   } else {
     store = configureStore();
   }
-
-  window.store = store;
+  // window.moment = moment;
+  // window.store = store;
 
   const main = document.getElementById('main');
   ReactDOM.render(<Root store={store}/>, main);

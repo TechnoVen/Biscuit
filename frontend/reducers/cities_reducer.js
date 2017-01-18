13b06b8de5cfb0848645151a0ffbb9673a15334b
laed37 <La.Edwin37@gmail.com>
2017-01-18 11:41:16 -0800

    Fix typos

--- frontend/reducers/cities_reducer.js
+++ frontend/reducers/cities_reducer.js
@@ -2,7 +2,7 @@ import {RECEIVE_CITY, RECEIVE_CITIES} from '../actions/city_actions';
 import merge from 'lodash/merge';
 
 const defaultState = {
-  cities: [],
+  all: [],
   currentCity: {}
 };
 
@@ -12,10 +12,10 @@ const citiesReducer = (oldState = defaultState, action) => {
 
   switch (action.type) {
     case RECEIVE_CITIES:
-      newState.cities = action.cities;
+      newState.all = action.cities;
       return newState;
     case RECEIVE_CITY:
-      newState.city = action.city;
+      newState.currentCity = action.city;
       return newState;
     default:
       return oldState;

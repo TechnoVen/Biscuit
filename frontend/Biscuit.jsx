68ea10fe2b367d17d68ce67919f6723a93512ba6
laed37 <La.Edwin37@gmail.com>
2017-01-18 17:49:16 -0800

    Update preloaded state to entry file

--- frontend/Biscuit.jsx
+++ frontend/Biscuit.jsx
@@ -2,24 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root';
 import configureStore from './store/store';
-import EventForm from './components/hosting/EventForm';
-import moment from 'moment';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
-    const preloadedState = {session: {
-      currentUser: window.currentUser,
-      hostProfile: null,
-      events: {},
-      errors: []
-    }};
+    const preloadedState = {
+      session: {
+        currentUser: window.currentUser,
+        hostProfile: null,
+        events: {},
+        errors: []
+      },
+      cities: {
+        all: [],
+        currentCity: {
+          events: [],
+          hosts: []
+        }
+      }
+    };
     store = configureStore(preloadedState);
   } else {
     store = configureStore();
   }
-  window.moment = moment;
-  window.store = store;
 
   const main = document.getElementById('main');
   ReactDOM.render(<Root store={store}/>, main);

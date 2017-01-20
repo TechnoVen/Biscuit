07d5ad18f99d81ffaac8c489c2f1e1cde712dd78
laed37 <La.Edwin37@gmail.com>
2017-01-19 16:58:16 -0800

    Remove multiple logos

--- frontend/components/app/Header.jsx
+++ frontend/components/app/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import HeaderNavLinks from './HeaderNavLinks';
-import {BiscuitLogo, BiscuitLogoColor1, BiscuitLogoColor2} from '../../svg/svg_graphics';
+import {BiscuitLogoColor2} from '../../svg/svg_graphics';
 
 const Header = ({signedIn, userCity, handleSignOut}) => {
   return (
@@ -9,21 +9,10 @@ const Header = ({signedIn, userCity, handleSignOut}) => {
       <div className="header-container">
         <Link className="header-logo-link" to='/'>
           <div className="logo-container">
-            <BiscuitLogo
-              viewBox="-10 -10 480 480"
-              height="50px"
-              width="50px"
-              stroke="white"
-              strokeWidth={20}
+            <BiscuitLogoColor2
+                height="50px"
+                width="50px"
             />
-          <BiscuitLogoColor1
-            height="50px"
-            width="50px"
-          />
-        <BiscuitLogoColor2
-            height="50px"
-            width="50px"
-          />
             biscuit
           </div>
         </Link>

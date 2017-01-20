48e4681c439982706aa201e3c0009ece52eeffb5
laed37 <La.Edwin37@gmail.com>
2017-01-19 17:49:03 -0800

    Separate components

--- frontend/components/cities/City.jsx
+++ frontend/components/cities/City.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import EventCard from '../EventCard';
+import CitySetHome from './CitySetHome';
 import {generate} from 'shortid';
-import {Link} from 'react-router';
 
 export default class City extends React.Component {
   constructor() {
@@ -74,55 +74,3 @@ export default class City extends React.Component {
     );
   }
 }
-
-const CitySetHome = ({updateUserCity, signedIn, cityId, userCity, myCity}) => {
-  const cities = {
-    1: 'bay area / sf',
-    2: 'los angeles',
-    3: 'new york',
-    4: 'chicago',
-    5: 'philadelphia',
-    6: 'san diego',
-  };
-
-  const validateCity = myCity ? '' : 'You must set this as your home city first!';
-
-  if (signedIn) {
-    if (userCity === cityId) {
-      return (
-        <div className="city-set-home">
-          <h2>This is currently your home city</h2>
-          <div>
-            You are free to join any event in this city. If you want to change or
-            view any other cities and their events, click on the button below.
-          </div>
-          <Link to="/cities">
-            <button>Set a different home city</button>
-          </Link>
-        </div>
-      );
-    } else {
-      return (
-        <div className="city-set-home">
-          <h2>Set your new home city below</h2>
-          <div>
-            You cannot join any events that are not in your home city.
-            Click below to set this as your home city soyou can join biscuit
-            meets here.
-          </div>
-          <div className="city-attendance-validation">
-            {validateCity}
-          </div>
-          <button onClick={updateUserCity}>
-            {`Set ${cities[cityId]} as my home city`}
-          </button>
-        </div>
-      );
-    }
-  }
-
-  return (
-    <div className="city-set-home">
-    </div>
-  );
-};

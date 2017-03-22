50250b3cdda356e836e97dd6e7549eeb39074f4a
laed37 <La.Edwin37@gmail.com>
2017-03-22 13:37:49 -0700

    fix header not displaying on different screen sizes

--- frontend/components/app/Header.jsx
+++ frontend/components/app/Header.jsx
@@ -3,34 +3,56 @@ import { Link } from 'react-router';
 import HeaderNavLinks from './HeaderNavLinks';
 import {BiscuitLogoColor2} from '../../svg/svg_graphics';
 
-const Header = ({signedIn, userCity, handleSignOut}) => {
-  return (
-    <header>
-      <div className="header-container">
-        <Link className="header-logo-link" to='/'>
-          <div className="logo-container">
-            <BiscuitLogoColor2
+export default class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show: false
+    };
+
+    this.showNavLinks = this.showNavLinks.bind(this);
+  }
+
+  showNavLinks() {
+    const show = !this.state.show;
+    this.setState({show});
+  }
+
+  render() {
+    const {signedIn, userCity, handleSignOut} = this.props;
+    const showMobileClass = this.state.show ? "header-nav-mobile mobile-nav" : "header-nav-mobile";
+    return (
+      <header>
+        <div className="header-container">
+          <Link className="header-logo-link" to='/'>
+            <div className="logo-container">
+              <BiscuitLogoColor2
                 height="50px"
                 width="50px"
-            />
-            biscuit
+                />
+              biscuit
+            </div>
+          </Link>
+          <div className="hide-nav">
+            <HeaderNavLinks
+              signedIn={signedIn}
+              userCity={userCity}
+              handleSignOut={handleSignOut}
+              />
           </div>
-        </Link>
-        <HeaderNavLinks
-          signedIn={signedIn}
-          userCity={userCity}
-          handleSignOut={handleSignOut}
-        />
-      </div>
-      <div className="header-nav-mobile">
-        <HeaderNavLinks
-          signedIn={signedIn}
-          userCity={userCity}
-          handleSignOut={handleSignOut}
-        />
-      </div>
-    </header>
-  );
-};
-
-export default Header;
+          <div onClick={this.showNavLinks} className="nav-icon-bars">
+            <i className="fa fa-2x fa-bars"></i>
+          </div>
+        </div>
+        <div className={showMobileClass}>
+          <HeaderNavLinks
+            signedIn={signedIn}
+            userCity={userCity}
+            handleSignOut={handleSignOut}
+            />
+        </div>
+      </header>
+    );
+  }
+}

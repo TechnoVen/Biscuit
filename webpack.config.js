84ef936b890661deb08295ee2d1f46ef0e588916
edwinla <La.Edwin37@gmail.com>
2017-05-01 18:55:36 -0700

    Add es7-syntax linting

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: 'babel',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015', 'react', ["stage-1"]],
+          plugins: ['transform-object-rest-spread']
         }
       },
       { test: /\.css$/,  loader: "style-loader!css-loader" },

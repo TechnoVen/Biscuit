94a946880ebb96bfba8ee013161c6e8e81afbd37
edwinla <La.Edwin37@gmail.com>
2017-05-24 18:04:15 -0700

    Add likes api

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,5 +1,14 @@
 class Api::LikesController < ApplicationController
+  before_action :require_signed_in!
+
   def index
-    @likes = Like.all
+    if params["likes"]
+      @likes = current_user.likes_me
+    elsif params["liked"]
+      @likes = current_user.likes_them
+    else
+      @likes = nil
+    end
+    render 'api/likes/index'
   end
 end

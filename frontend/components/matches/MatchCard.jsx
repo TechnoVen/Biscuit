8829ae6b2021425c32554a1c18e387d8527372b8
edwinla <La.Edwin37@gmail.com>
2017-05-30 23:05:50 -0700

    Add matchcard animations

--- frontend/components/matches/MatchCard.jsx
+++ frontend/components/matches/MatchCard.jsx
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const MatchCard = ({match}) => {
-  return (
-    <div className="match-card-container">
-      <div className="match-card">
-        <span className="match-card-image-wrapper">
-          <img className="match-card-image" src={match.image_url}/>
-        </span>
-        <div className="match-card-desc">
-          <div className="match-card-profile">
-            <span className="pet-name">{match.pet_name}</span>
-            <div className="pet-info">
-              <span className="breed">{match.breed}</span>
-              <span className="dot">·</span>
-              <span className="location">San Francisco, CA</span>
+class MatchCard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hover: false
+    };
+  }
+
+  toggleHoverClasses = () => {
+    this.setState((prevState) => {
+      return {hover: !prevState.hover};
+    });
+  }
+
+  render () {
+    const {match} = this.props;
+    const {hover} = this.state;
+    return (
+      <div className="match-card-container"
+        onMouseEnter={this.toggleHoverClasses}
+        onMouseLeave={this.toggleHoverClasses}>
+        <div className="match-card">
+          <span
+            className={'match-card-image-wrapper' + (hover ? ' hover-mciw' : '')}>
+            <img
+              className={"match-card-image" + (hover ? ' hover-mci' : '')}
+              src={match.image_url}/>
+          </span>
+          <div className="match-card-desc">
+            <div className={'match-card-profile' + (hover ? ' hover-mcp' : '')}>
+              <span className="pet-name">{match.pet_name}</span>
+              <div className="pet-info">
+                <span className="breed">{match.breed}</span>
+                <span className="dot">·</span>
+                <span className="location">San Francisco, CA</span>
+              </div>
             </div>
-          </div>
-          <div className="match-card-like">
-            <button className="match-card-button-like">
-              <i className="fa fa-lg fa-star"></i>
-              <span>like</span>
+            <div className="match-card-like">
+              <button className="match-card-button-like">
+                <i className="fa fa-star"></i>
+                <span>like</span>
               </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default MatchCard;

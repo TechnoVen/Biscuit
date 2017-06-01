f398e7ccb1c6436ef57001fafef6b640dd3975a6
edwinla <La.Edwin37@gmail.com>
2017-06-01 16:50:40 -0700

    Adjust header with new title outline

--- frontend/components/app/Header.jsx
+++ frontend/components/app/Header.jsx
@@ -1,24 +1,35 @@
 import React, {Component} from 'react';
 import {BiscuitLogo} from '../../svg/svg_graphics';
-const Header = ({signout, currentUser}) => {
-  return (
-    <div className="header-container">
+class Header extends Component {
+  constructor() {
+    super();
+  }
+
+  addStroke = (element) => {
+    element.setAttribute('title', element.innerHTML)
+  }
+
+  render() {
+    const {signout} = this.props;
+    return (
+      <div className="header-container">
         <div className="header-left">
-            <BiscuitLogo width="35" height="35" strokeWidth={15} />
-            <h1>Biscuit</h1>
+          {/*<BiscuitLogo width="35" height="35" strokeWidth={15} />*/}
+          <h1 className="single-stroke" ref={this.addStroke}>biscuit</h1>
         </div>
         <div className="header-right">
-            <a href="#">Dashboard</a>
-            {/*<img
-              alt={`user ${currentUser.email}`}
-              className="header-user-image"
-              src={currentUser.image_url}
+          <a href="#">Dashboard</a>
+          {/*<img
+            alt={`user ${currentUser.email}`}
+            className="header-user-image"
+            src={currentUser.image_url}
             />*/}
-          <span className="header-signout" onClick={signout}>Signout</span>
+            <span className="header-signout" onClick={signout}>Signout</span>
+          </div>
         </div>
-    </div>
-  );
-};
+      );
+  }
+}
 
 
 export default Header;
